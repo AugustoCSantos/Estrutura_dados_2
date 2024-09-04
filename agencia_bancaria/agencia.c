8650ef4bea3faa8eab61e2555b67012a29e1b483
Augusto Santos <augustocsantos.dev@gmail.com>
2024-09-04 20:35:39 -0300

    .c dos headers

--- agencia_bancaria/agencia.c
+++ agencia_bancaria/agencia.c
@@ -3,3 +3,50 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
+void imprime(Agencia *ag){
+    printf("-----------------------------------");
+    printf("\nCodigo da Agencia: ");
+    printf("%d", ag->codigo);
+    printf("\nNome da Agencia: ");
+    printf("%s", ag->nome);
+    printf("\nNome do Gerente: ");
+    printf("%s", ag->gerente);
+    printf("-----------------------------------\n");
+}
+
+Agencia* agencia(int codigo, char* nome, char* gerente){
+    Agencia *ag = (Agencia *) malloc(sizeof(Agencia));
+
+    if(ag) memset(ag, 0, sizeof(Agencia));
+
+    ag->codigo = codigo;
+    strcpy(ag->nome, nome);
+    strcpy(ag->gerente, gerente);
+
+    return ag;
+}
+
+void salva(Agencia *ag, FILE *out){
+    fwrite(&ag->codigo, sizeof(int), 1, out);
+    fwrite(ag->nome, sizeof(char), sizeof(ag->nome), out);
+    fwrite(ag->gerente, sizeof(char), sizeof(ag->gerente), out);
+}
+
+Agencia *le(FILE *in){
+    Agencia *ag = (Agencia *) malloc(sizeof(Agencia));
+
+    if(0 >= fread(&ag->codigo, sizeof(int), 1, in)){
+        free(ag);
+        return NULL;
+    }
+    fread(ag->nome, sizeof(char), sizeof(ag->nome), in);
+    fread(ag->gerente, sizeof(char), sizeof(ag->gerente), in);
+
+    return ag;
+}
+
+int tamanho(){
+    return sizeof(int)
+            + sizeof(char) * 50
+            + sizeof(char) * 50;
+}
---
+++ agencia_bancaria/conta.c
@@ -0,0 +1,51 @@
+#include "conta.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void imprime(Conta *cont){
+    printf("-----------------------------------");
+    printf("\nCodigo da Conta: ");
+    printf("%d", cont->codigo);
+    printf("\nCodigo da Agencia: ");
+    printf("%d", cont->codAgencia);
+    printf("\nSaldo: ");
+    printf("%2.lf", cont->saldo);
+    printf("-----------------------------------\n");
+}
+
+Conta* conta(int codigo, int codAgencia, double saldo){
+    Conta *cont = (Conta *) malloc(sizeof(Conta));
+
+    if(cont) memset(cont, 0, sizeof(Conta));
+    cont->codigo = codigo;
+    cont->codAgencia = codAgencia;
+    cont->saldo = saldo;
+
+    return cont;
+}
+
+void salva(Conta *cont, FILE *out){
+    fwrite(&cont->codigo, sizeof(int), 1, out);
+    fwrite(&cont->codAgencia, sizeof(int), 1, out);
+    fwrite(&cont->saldo, sizeof(double), 1, out);
+}
+
+Conta *le(FILE *in){
+    Conta *cont = (Conta *) malloc(sizeof(Conta));
+
+    if(0 >= fread(&cont->codigo, sizeof(int), 1, in)){
+        free(cont);
+        return NULL;
+    }
+    fread(&cont->codAgencia, sizeof(int), 1, in);
+    fread(&cont->saldo, sizeof(double), 1, in);
+
+    return cont;
+}
+
+int tamanho(){
+    return sizeof(int)
+            + sizeof(int)
+            + sizeof(double);
+}
