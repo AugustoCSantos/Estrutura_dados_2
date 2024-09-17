cb18370a23ccc0619dc08599c07198d83694813d
Augusto Santos <augustocsantos.dev@gmail.com>
2024-09-16 22:07:51 -0300

    ...

---
+++ arvore_vencedora/winnerTree.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_ARCHIVE 20
+#define HV 10000
+
+typedef struct{
+    int valor;
+    int indice;
+}registro;
---
+++ classificacao_externa/selecaoSub.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MEM_SIZE 6
+
