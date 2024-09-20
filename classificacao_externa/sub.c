9d39021ccda8b7109ce3ea87e0997d37ec119e1e
Augusto Santos <augustocsantos.dev@gmail.com>
2024-09-19 21:33:55 -0300

    sub

--- classificacao_externa/sub.c
+++ classificacao_externa/sub.c
@@ -5,7 +5,9 @@
 
 void escreve(FILE *out);
 void le(FILE *in);
-void selecao();
+void selecao(FILE *in, FILE *out);
+void insereHeap(int heap[], int *n, int valor);
+int remover(int heap[], int *n); 
 
 int main(){
     FILE *sequenciaReg = fopen("registros.dat", "w+b"); //arquivo desordenado.
@@ -26,7 +28,12 @@ int main(){
         return 1;
     }
 
+    selecao(sequenciaReg, saida);
+    rewind(saida);
+    le(saida);  //Ler os registros após a seleção por substituição
+
     fclose(sequenciaReg);
+    fclose(saida);
 
     return 0;
 }
@@ -48,4 +55,62 @@ void le(FILE *in){
     while(fread(&auxReg, sizeof(int), 1, in)){
         printf("%d\n", auxReg);
     }
+}
+
+void insereHeap(int heap[], int *n, int valor) {
+    int i = (*n)++; //Aumenta o tamanho do heap e define a posição do novo valor.
+
+    while (i > 0 && valor < heap[(i - 1) / 2]) {
+        heap[i] = heap[(i - 1) / 2];
+        i = (i - 1) / 2;
+    }
+    heap[i] = valor;
+}
+
+int remover(int heap[], int *n) {
+    int menor = heap[0];  //O menor valor é sempre o valor do topo (raiz).
+    int temp = heap[--(*n)];  //O último valor do heap é movido para o topo.
+    int i = 0, filho;
+
+    //Reorganiza o heap "descendo" o valor temporário (temp) até a posição correta.
+    while ((filho = 2 * i + 1) < *n) {  //Enquanto o filho esquerdo existir.
+        if (filho + 1 < *n && heap[filho + 1] < heap[filho]) {  //Se o filho direito for menor que o esquerdo.
+            filho++;
+        }
+        if (temp <= heap[filho]) break;  //Se o valor temporário já estiver no lugar certo.
+        heap[i] = heap[filho];  //Caso contrário, move o filho para cima
+        i = filho;  //continua descendo para o próximo nível.
+    }
+    heap[i] = temp;  //Coloca o valor temporário na posição correta.
+
+    return menor;  //Retorna o menor valor removido.
+}
+
+void selecao(FILE *in, FILE *out){
+    int heap[MEM_SIZE];
+    int tam_heap = 0;
+    int auxReg;
+    int marcador = -1;
+
+    for (int i = 0; i < MEM_SIZE; i++){
+        if(fread(&auxReg, sizeof(int), 1, in)){
+            insereHeap(heap, &tam_heap, auxReg);
+        }
+    } //preenche a heap com os registros
+
+    while (tam_heap > 0){
+        int menor = remover(heap, &tam_heap);
+        fwrite(&menor, sizeof(int), 1, out);
+
+        if (fread(&auxReg, sizeof(int), 1, in)) {
+            if (auxReg >= menor) {
+                insereHeap(heap, &tam_heap, auxReg);
+            } else { 
+                fwrite(&marcador, sizeof(int), 1, out);
+                insereHeap(heap, &tam_heap, auxReg);
+            } //Marcador de nova rodada.
+        }
+    }
+
+    fwrite(&marcador, sizeof(int), 1, out); //Marcar o fim da última rodada.
 }
